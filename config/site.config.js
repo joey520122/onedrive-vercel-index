8741af9ca52b4fe89bc635df04f4701d77254eaf
joey520122 <37426534+joey520122@users.noreply.github.com>
2023-02-11 10:35:38 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -17,7 +17,7 @@ module.exports = {
   kvPrefix: process.env.KV_PREFIX || '',
 
   // The name of your website. Present alongside your icon.
-  title: "Joeypi's OneDrive",
+  title: "JoeyPi's OneDrive",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/Public',
@@ -38,7 +38,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Powered by <a href="https://github.com/spencerwooo/onedrive-vercel-index" target="_blank" rel="noopener noreferrer">onedrive-vercel-index</a>. Made with ❤ by JoeyPi.',
+    'Powered by <a href="https://www.joeypi.top/" target="_blank" rel="noopener noreferrer">JoeyPi</a>. Made with ❤ by JoeyPi.',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
@@ -53,7 +53,7 @@ module.exports = {
   links: [
     {
       name: 'GitHub',
-      link: 'https://https://nas.joeypi.top:5001/',
+      link: 'https://github.com/joey520122/',
     },
   ],
 
