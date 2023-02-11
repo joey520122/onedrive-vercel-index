091b8d0edcbd43e9bfc2a5f1604755c0a2fff991
joey520122 <37426534+joey520122@users.noreply.github.com>
2023-02-11 10:45:00 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -38,7 +38,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Powered by <a href="https://www.joeypi.top/" target="_blank" rel="noopener noreferrer">JoeyPi</a>. Made with ❤ by JoeyPi.',
+    ' <a href="#" target="_blank" rel="noopener noreferrer">JoeyPi` Home</a>. Made with ❤ by JoeyPi.',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
